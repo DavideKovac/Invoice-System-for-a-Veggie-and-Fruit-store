a13d5c34e5ec92deba1f37ff4b1da5fd9bb8ff11
DavideKovac <47418599+DavideKovac@users.noreply.github.com>
2020-04-23 11:21:56 +0100

    Supplier class improved

--- Graded Unit/src/uk/ac/forthvalley/gradedunit/application/Supplier.java
+++ Graded Unit/src/uk/ac/forthvalley/gradedunit/application/Supplier.java
@@ -1,28 +1,44 @@
 package uk.ac.forthvalley.gradedunit.application;
 
+import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 public class Supplier {
+Connection con=null;
 private int supplierId=1;
-private String companyName;
-private String supplierEmail;
-private String supplierPhoneNumber;
-private PreparedStatement insertSupplier=con.preparedStatement("INSERT INTO suppliers VALUE(?,?,?,?)");
-private PreparedStatement getSupplierInfo=con.preparedStatement("SELECT supplier_id,company_name,email,phone_number FROM suppliers WHERE supplier_id=?");
+private String insertSupplierString="INSERT INTO suppliers VALUE(?,?,?,?)";
+private PreparedStatement insertSupplier=null;
+private String getSupplierInfoString="SELECT supplier_id,company_name,email,phone_number FROM suppliers WHERE supplier_id=?";
+private PreparedStatement getSupplierInfo=null;
 
 public Supplier()
 {
 	
 }
-public void insertNewSupplier(int supplierID,String companyName,String supplierEmail,String supplierPhoneNumber) {
-	insertSupplier.setInt(1,supplierID);
+public void insertNewSupplier(String companyName,String supplierEmail,String supplierPhoneNumber) throws SQLException{
+	try {
+	insertSupplier=con.prepareStatement(insertSupplierString);
+	insertSupplier.setInt(1,supplierId);
 	insertSupplier.setString(2,companyName);
 	insertSupplier.setString(3, supplierEmail);
 	insertSupplier.setString(4, supplierPhoneNumber);
-	supplierID++;
+	supplierId++;
+	}
+	catch(SQLException e)
+	{
+		e.getMessage();
+	}
 }
-public void getSupplierInfo(int supplierID) {
+public void getSupplierInfo(int supplierID) throws SQLException {
+	try {
+	getSupplierInfo=con.prepareStatement(getSupplierInfoString);
 	getSupplierInfo.setInt(1,supplierID);	
+	}
+	catch(SQLException e)
+	{
+		e.getMessage();
+	}
 }
 
 }
