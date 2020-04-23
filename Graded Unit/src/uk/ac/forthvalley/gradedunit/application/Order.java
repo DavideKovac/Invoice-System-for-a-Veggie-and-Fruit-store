9c31f4af843c4e0642f23364c015d258e3ff5428
DavideKovac <47418599+DavideKovac@users.noreply.github.com>
2020-04-23 11:17:56 +0100

    Order Class improved

--- Graded Unit/src/uk/ac/forthvalley/gradedunit/application/Order.java
+++ Graded Unit/src/uk/ac/forthvalley/gradedunit/application/Order.java
@@ -1,18 +1,39 @@
 package uk.ac.forthvalley.gradedunit.application;
+import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 public class Order {
+Connection con=null;
 private int orderID=0;
-private PreparedStatement insertNewOrder=con.preparedStatement("  ");
-private PreparedStatement getOrder=con.preparedStatement("SELECT * FROM orders WHERE order_id=?");
+private Product product=new Product();
+private String insertNewOrderString=" ";
+private PreparedStatement insertNewOrder=null;
+private String getOrderString="SELECT * FROM orders WHERE order_id=?";
+private PreparedStatement getOrder=null;
 
 public Order()
 {
 	
 }
-public void getOrder(int orderID)
+public void newOrder(String productName) throws SQLException
 {
+ boolean isAvailable=product.getProductAvailability(productName);
+ if(isAvailable==true)
+ {
+	 //add product
+ }
+ else
+ {
+	 //cant add product
+ }
+}
+
+public void getOrder(int orderID) throws SQLException
+{
+	getOrder=con.prepareStatement(getOrderString);
 	getOrder.setInt(1,orderID);
-	
+	 boolean isAvailable=false;
+	 
 }
 }
