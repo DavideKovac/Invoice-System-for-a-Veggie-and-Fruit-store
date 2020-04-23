fa196cfd1885dcd9d5dc1752c4179654a3e3d725
DavideKovac <47418599+DavideKovac@users.noreply.github.com>
2020-04-23 11:17:00 +0100

    Changed client class improved

--- Graded Unit/src/uk/ac/forthvalley/gradedunit/application/Client.java
+++ Graded Unit/src/uk/ac/forthvalley/gradedunit/application/Client.java
@@ -1,29 +1,45 @@
 package uk.ac.forthvalley.gradedunit.application;
 
+import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 public class Client {
+ private Connection con=null;
  private int clientID=1;
- private String companyName;
- private String companyEmail;
- private String clientPhoneNumber;
- private PreparedStatement insertClient=con.preparedStatement("INSERT INTO clients VALUES(?,?,?,?) ");
- private PreparedStatement getClientInfo=con.preparedStatement("SELECT client_id,company_name,e-mail,phone_number FROM clients WHERE client_id=?");
- 
+ private String insertClientString="INSERT INTO clients VALUES(?,?,?,?) ";
+ private PreparedStatement insertClient=null;
+ private String getClientInfoString="SELECT client_id,company_name,e-mail,phone_number FROM clients WHERE client_id=?";
+ private PreparedStatement getClientInfo=null;
  public Client()
  {
+	  
+ }
+
+ public void insertNewClient(String companyName,String companyEmail,String clientPhoneNumber) throws SQLException {
+	 try {
+		insertClient=con.prepareStatement(insertClientString);
+		insertClient.setInt(1,clientID);
+		insertClient.setString(2,companyName);
+	    insertClient.setString(3,companyEmail);
+	    insertClient.setString(4,clientPhoneNumber);
+	    clientID++;
+	} catch (SQLException e) {
+		System.out.println("Error in inserting the client");
+		e.getMessage();
+	}
 	 
  }
- public void insertNewClient(int clientID,String companyName,String companyEmail,String clientPhoneNumber) {
-	 insertClient.setInt(1,clientID);
-	 insertClient.setString(2,companyName);
-	 insertClient.setString(3,companyEmail);
-	 insertClient.setString(4,clientPhoneNumber);
-	 clientID++;
- }
-public void getClientInfo(int clientID)
+public void getClientInfo(int clientID) throws SQLException
 {
-  getClientInfo.setInt(1,clientID);	
+  try {
+	getClientInfo=con.prepareStatement(getClientInfoString);
+	getClientInfo.setInt(1,clientID);
+} catch (SQLException e) {
+	System.out.println("Error retriving client info");
+	e.getMessage();
+}	
 }
 
 }
+
