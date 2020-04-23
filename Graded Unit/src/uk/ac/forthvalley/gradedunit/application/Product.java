67633da7e32ae3ac042e7126a2b1b17c57191503
DavideKovac <47418599+DavideKovac@users.noreply.github.com>
2020-04-23 11:19:36 +0100

    Product class improved and changed

--- Graded Unit/src/uk/ac/forthvalley/gradedunit/application/Product.java
+++ Graded Unit/src/uk/ac/forthvalley/gradedunit/application/Product.java
@@ -1,23 +1,27 @@
 package uk.ac.forthvalley.gradedunit.application;
 
+import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class Product {
-private String productName;
-private int productQuantity;
-private double productPrice;
-private String productProvenance;
-private String productDescription;
-private boolean productType;
-private PreparedStatement insertNewProduct=con.preparedStatement("IF EXIST(SELECT * FROM product WHERE product_name=?)BEGIN UPDATE products SET quantity=quantity+? WHERE product_name=? END ELSE BEGIN INSERT INTO products VALUES(?,?,?,?,?,?,?) END");
-private PreparedStatement getProduct=con.preparedStatement("UPDATE products SET quantity=quantity-? WHERE product_name=?");
+private Connection con=null;
+private String checkProductAvailabilityString="SELECT quantity FROM product WHERE product_name=?";
+private PreparedStatement checkProductAvailability=null;
+private String insertNewProductString="IF EXIST(SELECT * FROM product WHERE product_name=?)BEGIN UPDATE products SET quantity=quantity+? WHERE product_name=? END ELSE BEGIN INSERT INTO products VALUES(?,?,?,?,?,?,?,?) END";
+private PreparedStatement insertNewProduct=null;
+private String getProductString="UPDATE products SET quantity=quantity-? WHERE product_name=?";
+private PreparedStatement getProduct=null;
 public Product()
 {
 	
 	
 }
-public void insertNewProduct(String productName,int productQuantity,double productPrice,String productProvenance,String productDescription,boolean productType,int supplierID)
-{ 
+public void insertNewProduct(String productName,int productQuantity,double productPrice,String productProvenance,String productDescription,boolean productType,int supplierID) throws SQLException
+{  
+	try {
+	insertNewProduct=con.prepareStatement(insertNewProductString);
 	insertNewProduct.setString(1,productName);
 	insertNewProduct.setInt(2, productQuantity);
 	insertNewProduct.setString(3, productName);
@@ -28,14 +32,47 @@ public void insertNewProduct(String productName,int productQuantity,double produ
 	insertNewProduct.setString(8, productDescription);
 	insertNewProduct.setBoolean(9, productType);
 	insertNewProduct.setInt(10,supplierID);
+	}
+	catch(SQLException e)
+	{
+		e.getMessage();
+	}
 }
 //Realize the need of change the class diagram depending on the number of product
 //adding two statement and two methods
-public void getProduct(String productName,int productQuantity)
+public void getProduct(String productName,int productQuantity) throws SQLException
 {
-	
+	try {
+	getProduct=con.prepareStatement(getProductString);
 	getProduct.setInt(1,productQuantity);
 	getProduct.setString(2, productName);
+	}
+	catch(SQLException e)
+	{
+		e.getMessage();
+	}
+}
+public boolean getProductAvailability(String productName)throws SQLException
+{
+	 boolean isAvailable=false;
+	 if(getProductQuantity(productName)>3)
+	 {
+		 isAvailable=true;
+	 }
+	 else if(getProductQuantity(productName)<=3)
+	 {
+		 isAvailable=false;
+	 }
+	 return isAvailable;
+}
+private int getProductQuantity(String productName) throws SQLException
+{
+	 checkProductAvailability=con.prepareStatement(checkProductAvailabilityString);
+	 checkProductAvailability.setString(1,productName);
+	 ResultSet rs=checkProductAvailability.executeQuery();
+	 int quantity=rs.getInt("quantity");
+	 return quantity;
 }
 }
+
 
