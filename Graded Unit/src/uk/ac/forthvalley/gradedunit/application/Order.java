d1db60f7c9b5075dfca670aabc8eec2b4b0d3f5b
DavideKovac <47418599+DavideKovac@users.noreply.github.com>
2020-04-24 13:39:13 +0100

    Finally arrived at a conclusion on Order Class
    Not sure if this is the best way but i was struggling with the implementation of adding order to the SQL DB, right now the application part is done

--- Graded Unit/src/uk/ac/forthvalley/gradedunit/application/Order.java
+++ Graded Unit/src/uk/ac/forthvalley/gradedunit/application/Order.java
@@ -5,35 +5,48 @@ import java.sql.SQLException;
 
 public class Order {
 Connection con=null;
-private int orderID=0;
+private int orderID=1;
 private Product product=new Product();
 private String insertNewOrderString=" ";
 private PreparedStatement insertNewOrder=null;
 private String getOrderString="SELECT * FROM orders WHERE order_id=?";
 private PreparedStatement getOrder=null;
+private String addToOrderedItemsString="INSERT productName,productID INTO ordered products";
+private PreparedStatement addToOrderedItems=null;
+private String addNewOrderString="INSERT orderID,clientID FROM orders";
+private PreparedStatement addNewOrder=null;
 
 public Order()
 {
 	
 }
-public void newOrder(String productName) throws SQLException
+public void newOrderProduct(String productName,int productQuantity) throws SQLException
 {
  boolean isAvailable=product.getProductAvailability(productName);
+ addToOrderedItems=con.prepareStatement(addToOrderedItemsString);
  if(isAvailable==true)
  {
-	 //add product
+	 product.getProduct(productName, productQuantity);
+	 addToOrderedItems.setString(1,productName);
+	 addToOrderedItems.setInt(2,orderID);
+	 
  }
  else
  {
-	 //cant add product
+	 System.out.println("Unavailable product");
  }
 }
+public void addOrder(int orderID,int clientID) throws SQLException
+{
+    addNewOrder=con.prepareStatement(addNewOrderString);	
+    addNewOrder.setInt(1, orderID);
+    addNewOrder.setInt(2, clientID);
 
+}
 public void getOrder(int orderID) throws SQLException
 {
 	getOrder=con.prepareStatement(getOrderString);
 	getOrder.setInt(1,orderID);
-	 boolean isAvailable=false;
 	 
 }
 }
