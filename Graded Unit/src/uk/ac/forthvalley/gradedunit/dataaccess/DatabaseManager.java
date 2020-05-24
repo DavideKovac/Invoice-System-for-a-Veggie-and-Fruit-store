89840ea219e87c4aea0ecc6bb1c2f7670ebce32c
DavideKovac <47418599+DavideKovac@users.noreply.github.com>
2020-05-24 13:28:31 +0100

    Final databasemanager class for the prototype

--- Graded Unit/src/uk/ac/forthvalley/gradedunit/dataaccess/DatabaseManager.java
+++ Graded Unit/src/uk/ac/forthvalley/gradedunit/dataaccess/DatabaseManager.java
@@ -1,37 +1,51 @@
 package uk.ac.forthvalley.gradedunit.dataaccess;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-
+import java.util.ArrayList;
 import uk.ac.forthvalley.gradedunit.application.*;
 import uk.ac.forthvalley.gradedunit.gui.menuUI;
-
+/**
+ * This is the class that connect the GUI(uk.ac.forthvallley.gradedunit.gui) and the data access(uk.ac.forthvalley.gradedunit.dataaccess)
+ * @author Davide Kovac 
+ *
+ */
 public class DatabaseManager {
-	final private String url= "database url"; 
-    final private String user = "user"; 
-    final private String password = "password"; 
-    Connection con=null;
+	/**
+	 * Client object from the data access package
+	 */
     Client client=new Client();
+    /**
+	 * Supplier object from the data access package
+	 */
     Supplier supplier=new Supplier();
+    /**
+	 * Product object from the data access package
+	 */
     Product product=new Product();
+    /**
+	 * Report object from the data access package
+	 */
     Report report=new Report();
+    /**
+	 * Order object from the data access package
+	 */
     Order order=new Order();
+    /**
+	 * MenuUI object from the GUI package
+	 */
     menuUI gui = new menuUI();
-    int orderID=1;
     public DatabaseManager()
     {}
-    
-	 public String getClient(int clientID)
+    /**
+     *<p>Connect the GUI and the data access to execute the getClientInfo() method in Client class</p>
+     * @param clientID got from the GUI
+     * @return Return the information from the client class that the method got from the database and send them to the GUI
+     */
+	 public String getClient(int clientID) 
 	{
-		 String clientString="";
+		 String clientString=null;
 		try {
-			con=DriverManager.getConnection(url, user, password);
-			client.getClientInfo(clientID);
 			clientString=client.getClientInfo(clientID);
-			con.close();
-			
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -40,108 +54,138 @@ public class DatabaseManager {
 		return clientString;
 	}
 	 
-	
-	 public void insertClient(String companyName,String companyEmail,String clientPhoneNumber)
-		{
-			
+	/**
+	 * <p>Conncet the GUI and the dataacces to execute the Client.insertNewClient() method</p>
+	 * @param companyName got from the GUI
+	 * @param companyEmail got from the GUI
+	 * @param clientPhoneNumber got from the GUI
+	 */
+	 public void insertClient(String companyName,String companyEmail,String clientPhoneNumber) 
+	    {	
 			try {
-				con=DriverManager.getConnection(url, user, password);
 				client.insertNewClient(companyName, companyEmail, clientPhoneNumber);
-				con.close();
 			} catch (SQLException e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 		}
-	 
-	 public String getSupplier(int supplierID)
+	 /**
+	  * <p>Connect the GUI and the dataacces to execute the Supplier.getSupplierInfo() method</p>
+	  * @param supplierID got from the GUI
+	  * @return Return the information from the supplier class that the method got from the database and send them to the GUI
+	  */
+	 public String getSupplier(int supplierID) 
 	 {
 		 String supplierInfo="";
 		 try {
-			 con=DriverManager.getConnection(url, user, password);
-			 supplier.getSupplierInfo(supplierID);
 			 supplierInfo=supplier.getSupplierInfo(supplierID);
-			 con.close();
 			 }catch(SQLException e) {
 			e.printStackTrace(); 
 		 }
 		 return supplierInfo;
 	 }
-	 
-	 public void insertNewSupplier(String companyName,String supplierEmail,String supplierPhoneNumber)
+	 /**
+	  * <p>Connect the GUI and the dataacces to execute the Supplier.insertNewSupplier() method</p>
+	  * @param companyName got from the GUI
+	  * @param supplierEmail got from the GUI
+	  * @param supplierPhoneNumber got from the GUI
+	  */
+	 public void insertNewSupplier(String companyName,String supplierEmail,String supplierPhoneNumber) 
 	 {
 		 try {
-			 con=DriverManager.getConnection(url, user, password);
 			 supplier.insertNewSupplier(companyName, supplierEmail, supplierPhoneNumber);
-			 con.close();
 		 }catch(SQLException e) {
 			e.printStackTrace(); 
 		 }
 	 }
-	 
-	 public void insertNewProduct(String productName,int productQuantity,double productPrice,String productProvenance,String productDescription,boolean productType,int supplierID)
+	 /**
+	  * <p>Connect the GUI and the dataacces to execute the Product.insertNewProduct() method</p>
+	  * @param productName got from the GUI
+	  * @param productQuantity got from the GUI
+	  * @param productPrice got from the GUI
+	  * @param productProvenance got from the GUI
+	  * @param productDescription got from the GUI
+	  * @param productType got from the GUI
+	  * @param supplierID got from the GUI
+	  */
+	 public void insertNewProduct(String productName,int productQuantity,double productPrice,String productProvenance,String productDescription,int productType,int supplierID) 
 	 {
 		 try {
-			 con=DriverManager.getConnection(url, user, password);
 			 product.insertNewProduct(productName, productQuantity, productPrice, productProvenance, productDescription, productType, supplierID);
-			 con.close();
 		 }catch(SQLException e) {
 			e.printStackTrace(); 
 		 }
 	 }
-	 
-	 public void getWeeklyReport()
-	 {
+	 /**
+	  * <p>Connect the GUI and the dataacces to execute the Report.getWeeklyReport() method </p>
+	  * @return Return the information from the report class that the method got from the database and send them to the GUI
+	  */
+	 public ArrayList<String> getWeeklyReport() 
+	 {  
+	    ArrayList <String> weeklyReport=new ArrayList<>();
 		 try {
-			 con=DriverManager.getConnection(url, user, password);
-			 report.getWeeklyReport();
-			 con.close();
+			 weeklyReport=report.getWeeklyReport();
 		 }catch(SQLException e) {
 			e.printStackTrace(); 
 		 }
+		return weeklyReport;
 	 }
-	 
-	 public void getMonthlyReport()
+	 /**
+	  * <p>Connect the GUI and the dataacces to execute the Report.getMonthlyReport() method </p>
+	  * @return  Return the information from the report class that the method got from the database and send them to the GUI
+	  */
+	 public ArrayList<String> getMonthlyReport() 
 	 {
+		 ArrayList <String> monthlyReport=new ArrayList<>();
 		 try {
-			 con=DriverManager.getConnection(url, user, password);
-			 report.getMonthlyReport();
-			 con.close();
+			monthlyReport=report.getMonthlyReport();
 		 }catch(SQLException e) {
 			e.printStackTrace(); 
 		 }
+		return monthlyReport;
 	 }
-    
-	 public void addOrder(String productName,int productQuantity,int clientID){
+    /**
+     * <p>Connect the GUI and the dataacces to execute the Order.addOrder() method</p>
+     * @param clientID got from the GUI
+     */
+	 public void addOrder(int clientID) {
 
 		 try { 
-			 con=DriverManager.getConnection(url, user, password);
-             order.addOrder(orderID, clientID);
-			order.newOrderProduct(productName, productQuantity,orderID);
-			con.close();
+             order.addOrder( clientID);
 		}catch(SQLException e) {
 				 e.printStackTrace();	 
 		}
 		 
 	 }
-	 
-	 public String getOrder(int orderID)
+	 /**
+	  * <p>Connect the GUI and the dataacces to execute the Order.getOrder() method</p>
+	  * @param orderID got from the GUI
+	  * @return  Return the information from the report class that the method got from the database and send them to the GUI
+	  */
+	 public String getOrder(int orderID) 
 	 {
 		 String orderString="";
 		 try {
-			 con=DriverManager.getConnection(url, user, password);
-			 order.getOrder(orderID);
 			 orderString=order.getOrder(orderID);
-			 con.close();
 		 }catch(SQLException e) {
 			e.printStackTrace(); 
 		 }
-		 return orderString;
+		 return orderString;                                                  
 	 }
-	 public void incrementOrderID()
+	 /**
+	  * <p>Connect the GUI and the dataacces to execute the Order.newOrderedProduct() method</p>
+	  * @param productName got from the GUI
+	  * @param productQuantity got from the GUI
+	  */
+	 public void addToOrderedItems(String productName, int productQuantity)
 	 {
-		 orderID++;
+		 try {
+			 order.newOrderProduct(productName, productQuantity);
+		 }
+		 catch(SQLException e)
+		 {
+			 e.printStackTrace();
+		 }
 	 }
 	 
-	 
+	
 }
